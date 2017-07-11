80f99bb5d085765351b5ef53c23f04b02cc93c18
Raphael Ferreira <raphaelbsferreira@gmail.com>
2017-07-11 00:19:12 -0300

    Mongo atlas url

--- tools/config.js
+++ tools/config.js
@@ -1,4 +1,4 @@
 export default {
-  DB_URL: "mongodb://192.168.99.100:32770",
+  DB_URL: "mongodb://deeto:UfgDeeto123@raphaelferreira-shard-00-00-kjo3n.mongodb.net:27017,raphaelferreira-shard-00-01-kjo3n.mongodb.net:27017,raphaelferreira-shard-00-02-kjo3n.mongodb.net:27017/deeto?ssl=true&replicaSet=RaphaelFerreira-shard-0&authSource=admin",
   SECRET: "RAPHAELFERREIRA"
 };
