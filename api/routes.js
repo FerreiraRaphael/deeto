b7478f1acf02c4b3c478f1cde5d065943ea0db99
Rony Nogueira <ronynogueiras.dev@gmail.com>
2017-07-13 12:43:03 -0300

    atualização de página inicial

--- api/routes.js
+++ api/routes.js
@@ -2,11 +2,17 @@ import { Router } from "express";
 import path from "path";
 import user from "./models/User/routes";
 import match from "./models/Match/routes";
+import path from "path";
 
 const router = new Router();
+const views = path.resolve("public", "views") + path.sep;
 
 router.route("/").get((req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+router.route("/").get((req, res) => {
+  res.sendFile(views + "index.html");
 });
 
 router.use("/api/users", user);
